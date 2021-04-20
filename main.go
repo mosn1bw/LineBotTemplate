d3b3eb201a6df2bd78ea850ad298947987f32aea
mosn1bw <81889769+mosn1bw@users.noreply.github.com>
2021-04-21 03:59:03 +0430

    Update main.go

--- main.go
+++ main.go
@@ -46,15 +46,9 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-// Text message
-func Text(text string) *linebot.TextMessage {
-	return linebot.NewTextMessage(text)
-}
-
 	if keyword == "e3e" {
 		messages := []linebot.Message{
 			linebot.NewTextMessage("友達登録ありがとうございます。"),
-			linebot.NewTextMessage(message.ID+":"+message.Text+" OK! remain message:"+strconv.FormatInt(quota.Value, 10)).Do(); err != nil {}
 		}
 	}
 
