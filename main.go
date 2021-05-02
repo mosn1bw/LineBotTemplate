62edcb3387076986a7f5e480bdf205e09ca0b0d4
mosn1bw <81889769+mosn1bw@users.noreply.github.com>
2021-05-02 11:40:31 +0430

    Update main.go

--- main.go
+++ main.go
@@ -1,78 +1,59 @@
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package main
+package function
 
 import (
-	"fmt"
+	"context"
 	"log"
-	"net/http"
-	"os"
-	"strconv"
 
-	"github.com/line/line-bot-sdk-go/linebot"
+	"github.com/gcp-kit/line-bot-gcp-go/gcpine"
+	"github.com/go-utils/caller"
+	"github.com/line/line-bot-sdk-go/v7/linebot"
+	"golang.org/x/xerrors"
 )
 
-var bot *linebot.Client
+// TextEvent - handle text message events
+func TextEvent(ctx context.Context, pine *gcpine.GCPine, event *linebot.Event) ([]linebot.SendingMessage, error) {
+	name := caller.GetCallFuncName()
+	log.Println("Call:", name)
 
-func main() {
-	var err error
-	bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
-	log.Println("Bot:", bot, " err:", err)
-	http.HandleFunc("/callback", callbackHandler)
-	port := os.Getenv("PORT")
-	addr := fmt.Sprintf(":%s", port)
-	http.ListenAndServe(addr, nil)
-}
-
-func callbackHandler(w http.ResponseWriter, r *http.Request) {
-	events, err := bot.ParseRequest(r)
-
-	if err != nil {
-		if err == linebot.ErrInvalidSignature {
-			w.WriteHeader(400)
-		} else {
-			w.WriteHeader(500)
-		}
-		return
+	message, ok := event.Message.(*linebot.TextMessage)
+	if !ok {
+		return nil, xerrors.New("couldn't cast")
 	}
-		
-	case "a8":
-		if len(profile) > 0{
-			bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(profile[2] + "你已經是菜市場的會員囉，不用再申請加入啦")).Do()
-			bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(profile[2] + "你已經是菜市場的會員囉，不用再申請加入啦")).Do()
-			bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(profile[2] + "你已經是菜市場的會員囉，不用再申請加入啦")).Do()
-		}
-				
-	case "e3e" {
-		messages := []linebot.Message{
-			linebot.NewTextMessage("友達登録ありがとうございます。"),
-			case "m3": bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("老大姓張不姓謝喔")).Do()
-			case "m3": bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("老大姓張不姓謝喔")).Do()
-			}
+
+	text := message.Text
+
+	replyMessage := linebot.SendingMessage(linebot.NewTextMessage(text))
+
+	if text == "ping" {
+		items := &linebot.QuickReplyItems{
+			Items: []*linebot.QuickReplyButton{
+				{
+					Action: linebot.QuickReplyAction(&linebot.MessageAction{
+						Label: "ping",
+						Text:  "ping",
+					}),
+				},
+			},
 		}
 
-	for _, event := range events {
-		if event.Type == linebot.EventTypeMessage {
-			switch message := event.Message.(type) {
-			case *linebot.TextMessage:
-				quota, err := bot.GetMessageQuota().Do()
-				if err != nil {
-					log.Println("Quota err:", err)
-				}
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK! remain message:"+strconv.FormatInt(quota.Value, 10))).Do(); err != nil {
-					log.Print(err)
-				}
-			}
+		prof, err := pine.GetProfile(event.Source.UserID).WithContext(ctx).Do()
+		if err != nil {
+			return nil, xerrors.Errorf("error in GetProfile method: %w", err)
 		}
+
+		sender := &linebot.Sender{
+			Name:    prof.DisplayName,
+			IconURL: prof.PictureURL,
+		}
+
+		replyMessage = linebot.NewTextMessage("pong").
+			WithQuickReplies(items).
+			WithSender(sender)
 	}
+
+	stack := []linebot.SendingMessage{
+		replyMessage,
+	}
+
+	return stack, nil
 }
